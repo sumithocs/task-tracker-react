87fbb1de1d5c4fa16315d6d0d4615537adab3c9b
Sumith Samaraweerahetti <sumithocs@gmail.com>
2021-08-25 15:55:02 +1200

    before adding the backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+import { useState } from "react"
 
 function App() {
+  
+  const [showAddTask, setShowAddTask] = useState(false)
+
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1
+    //console.log(id)
+    //return
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])    
+  }
+
+  const deleteTask = (id) => {
+    //console.log('delete task', id);
+    setTasks(tasks.filter((task)=> task.id !== id))
+  }
+
+  const toggleTask = (id) => {
+    console.log('toggle task reminder', id);  
+    setTasks(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))  
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> 
+      <Header title={'Task Tracker App'} onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} /> 
+      {/*Show Add Task ternary*/}
+      {showAddTask && <AddTask onAdd={addTask} />} 
+      {tasks.length >0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleTask}/> : 'No Tasks To Show'}      
     </div>
   );
 }
